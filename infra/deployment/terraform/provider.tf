35ddcd6249ccfc4eec7037157b101b08569cc733
hacksungboo <qngkrtjd@naver.com>
2026-05-17 15:47:27 +0900

    ci/cd 테스트

--- infra/deployment/terraform/provider.tf
+++ infra/deployment/terraform/provider.tf
@@ -1,7 +1,7 @@
 # infra/deployment/terraform/provider.tf
 # AWS에 Web, Rec 서버를 provisioning한다.
 
-# 2026.05.15 16:10 CI/CD 테스트용 주석 수정
+# 2026.05.17 15:47 CI/CD 테스트용 주석 수정
 
 terraform {
   required_version = ">= 1.14.0, < 2.0.0"
